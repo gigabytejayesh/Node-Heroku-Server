075d31714ab7e91695da5db5509711846bfbfdaa
gigabytejayesh <jayeshwadibhasme0697@gmail.com>
2021-05-10 00:43:42 +0530

    Removed host access to the server.

--- src/manifest.ts
+++ src/manifest.ts
@@ -2,7 +2,7 @@ const Config = require("config");
 
 export = {
     server: {
-        host: process.env.HOST || process.env.host || "smart-hm.herokuapp.com",
+        // host: process.env.HOST || process.env.host || "smart-hm.herokuapp.com",
         port: process.env.PORT || 80,
         // host: "localhost",
         // port: 8080,
