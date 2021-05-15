78f87ed01335157f843b1eb1d3ae5c4ff1c8e340
gigabytejayesh <jayeshwadibhasme0697@gmail.com>
2021-05-15 19:00:17 +0530

    Update HTML UI pages

--- public/script.js
+++ public/script.js
@@ -1,16 +1,21 @@
-gsap.from(".u-text", { duration: 1, x: "-100%" });
-
 var tl = new TimelineMax({ repeat: -1, yoyo: true });
 tl.fromTo(
-    ".u-image-1",
-    1,
+    ".add-swing",
+    1.5,
     { ease: "circ.inOut", y: 10 },
     { ease: "circ.inOut", y: -10 }
 );
 
 tl.fromTo(
-    ".u-image-1",
-    1,
+    ".add-swing",
+    1.5,
     { ease: "circ.inOut", y: -10 },
     { ease: "circ.inOut", y: 10 }
 );
+
+gsap.from(".u-text", { duration: 1, x: "-100%" });
+gsap.from(".u-btn-1", {
+    duration: 1,
+    opacity: 0,
+    delay: 1,
+});
