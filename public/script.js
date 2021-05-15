9da43b947a20ca553901628b6c0cf664cccc6505
gigabytejayesh <jayeshwadibhasme0697@gmail.com>
2021-05-15 19:07:52 +0530

    Updated index.html to add title and favicon

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var tl = new TimelineMax({ repeat: -1, yoyo: true });
+var tl = new TimelineMax({ repeat: -1, yoyo: true, opacity: 0 });
 tl.fromTo(
     ".add-swing",
     1.5,
@@ -19,3 +19,5 @@ gsap.from(".u-btn-1", {
     opacity: 0,
     delay: 1,
 });
+
+gsap.from(".u-image-1", { duration: 1, opacity: 0, delay: 0.5 });
